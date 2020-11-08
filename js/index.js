a311aaf42c4321da4120a12466f093e059650fb5
websolutions700 <74103962+websolutions700@users.noreply.github.com>
2020-11-09 00:55:41 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,35 @@
+window.onload = ()=> {
+	
+	document.querySelector('nav i.fa').onclick = ()=> {
+				document.querySelector('.mobile-navbar').classList.toggle('active');
+				if (document.querySelector('.mobile-navbar').classList.contains('active')) {
+					document.querySelector('nav i.fa').classList.replace('fa-bars', 'fa-times');
+				} else {
+					document.querySelector('nav i.fa').classList.replace('fa-times', 'fa-bars');
+				}
+			}
+
+	document.querySelector('.mode-toggle').onclick = function() {
+		if (this.querySelector('i').classList.contains('fa-moon-o')) {
+			this.querySelector('i').classList.replace('fa-moon-o', 'fa-sun-o');
+			document.body.style.setProperty('--pcol', '#232323');
+			document.body.style.setProperty('--scol', '#eee');
+			document.body.style.setProperty('--shtcol', '#2b2b2b');
+			document.body.style.setProperty('--shbcol', '#1a1a1a');
+
+		} else {
+			this.querySelector('i').classList.replace('fa-sun-o', 'fa-moon-o');
+			document.body.style.setProperty('--pcol', '#f1f1f1');
+			document.body.style.setProperty('--scol', '#554');
+			document.body.style.setProperty('--shtcol', '#fff');
+			document.body.style.setProperty('--shbcol', '#ddd');
+		}
+	}
+
+	document.querySelector('nav p.head').onclick = ()=> {
+		window.location = 'index.html';
+	}
+
+	
+
+}
